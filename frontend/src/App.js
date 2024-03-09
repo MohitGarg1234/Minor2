373d9c21ce49b00c0e83c346572269844c69a776
Mohit Garg <mgarg1557@gmail.com>
2024-03-09 14:53:46 +0530

    Login Signup UI Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import  { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Signup from './Components/Signup';
+import Login from './Components/Login';
 function App() {
   return (
     <div>
@@ -9,6 +11,8 @@ function App() {
       <Router>
         <Routes>
           <Route  path="/" element={<Home/>}/>
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/login" element={<Login/>} />
         </Routes>
       </Router>
     </div>
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "../App.css";
+import {Link} from  'react-router-dom'
 const Home = () => {
   return (
     <div>
       
       <div className="header">
-        <h1 className="headerHeading">Convert With Your Own Network</h1>
-        <button className="btn">LOGIN</button>
-        <button className="btn">SIGNUP</button>
+        <h1 className="headerHeading">Connect With Your Own Network</h1>
+        <button className="btn">
+          <Link to="/login" >LOGIN</Link>
+        </button>
+        <button className="btn">
+          <Link to="/signup">SIGNUP</Link>
+        </button>
       </div>
     </div>
   );
