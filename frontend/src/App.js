f0ac0142b4238064a1ee315ff37fcd8868314621
Mohit Garg <mgarg1557@gmail.com>
2024-03-11 19:57:44 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import Header from './Components/Header';
+// import Header from './Components/Header';
 import Home from './Components/Home';
 import  { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Signup from './Components/Signup';
 import Login from './Components/Login';
 import Verification from './Components/Verification';
 import Details from './Components/Details';
+import HomePage from './Components/HomePage';
+import Navbar from './Components/Navbar';
 function App() {
   return (
-    <div>
-      <Header/>
+    <div >
+      {/* <Header/> */}
+      <Navbar/>
       <Router>
         <Routes>
           <Route  path="/" element={<Home/>}/>
@@ -17,6 +20,7 @@ function App() {
           <Route path="/login" element={<Login/>} />
           <Route path="/verification" element={<Verification/>}/>
           <Route path="/details" element={<Details/>}/>
+          <Route path='/homepage' element={<HomePage/>}/>
         </Routes>
       </Router>
     </div>
