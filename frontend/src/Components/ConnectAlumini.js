dd122caeee3bb9517666f6396866f13252937603
Chiranshu Agrawal <agarwalchiranshu2003@gmail.com>
2024-03-18 18:19:05 +0530

    Added Connect Alumini Page

---
+++ frontend/src/Components/ConnectAlumini.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import Logo from "../images/Logo-jiit.png";
+const ConnectAlumini = () => {
+  return (
+    <>
+    <div className="flex">
+    <form class="max-w-md mx-auto">   
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type="search" id="default-search" class="mb-2 block w-60 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Alumini" required />
+        </div>
+    </form>
+    <form class="max-w-md mx-auto">   
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type="search" id="default-search" class=" mb-2 block w-60 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Alumini" required />
+        </div>
+    </form>
+    </div>
+
+    <div className='container'>
+         <a href="#" class="mb-3 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={Logo} alt=""/>
+            <div class="flex-col p-4 leading-normal">
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Chiranshu Agrawal</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Graduation Year- 2025</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Profile- Software Developer II</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Company- Apni Company</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Experience- 5 Years</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Past Experience- hai hi nhu</h6>
+            </div>
+        </a>
+
+        <a href="#" class="mb-3 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={Logo} alt=""/>
+            <div class="flex-col p-4 leading-normal">
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Chiranshu Agrawal</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Graduation Year- 2025</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Profile- Software Developer II</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Company- Apni Company</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Experience- 5 Years</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Past Experience- hai hi nhi</h6>
+            </div>
+        </a>
+
+        <a href="#" class="mb-3 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={Logo} alt=""/>
+            <div class="flex-col p-4 leading-normal">
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Chiranshu Agrawal</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Graduation Year- 2025</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Profile- Software Developer II</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Company- Apni Company</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Experience- 5 Years</h6>
+                <h6 class="m-2 font-bold tracking-tight text-gray-900 dark:text-white">Past Experience- hai hi nhu</h6>
+            </div>
+        </a>
+
+    </div>
+    </>
+  )
+}
+
+export default ConnectAlumini
