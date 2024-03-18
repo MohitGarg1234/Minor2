4c22ac9fe6f1ed65fa246c5f07482f13923d1173
Chiranshu Agrawal <agarwalchiranshu2003@gmail.com>
2024-03-18 18:21:30 +0530

    Home Page Added

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -1,11 +1,29 @@
-import React from 'react'
+// HomePage.js
+import React from 'react';
+import './HomePage.css';
 
 const HomePage = () => {
   return (
-    <div>
-        HELLO
+    <div className="centered-container">
+      <div>
+        <div className="card-container">
+          <div className="card-body">
+            <div className="card-title">
+              <img className="person-photo" src="" alt="person face" />
+              <h5 className="person-name">Chiranshu</h5>
+            </div>
+            <p className="card-text">
+              This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            </p>
+            <p className="card-text">
+              <small className="text-body-secondary">Last updated 3 mins ago</small>
+            </p>
+          </div>
+          <img className="card-img-bottom" src="" alt="post" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
