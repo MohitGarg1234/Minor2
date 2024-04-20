a4a174cc9c47aac11e0af2cd3e19a18c2eb4a469
Chiranshu Agrawal <agarwalchiranshu2003@gmail.com>
2024-04-20 16:39:45 +0530

    Home Page Updated

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -1,6 +1,7 @@
-// HomePage.js
 import React from 'react';
 import './HomePage.css';
+import Logo from "../images/Logo-jiit.png";
+import Logo1 from "../images/Jiit-Ground.jpeg"
 
 const HomePage = () => {
   return (
@@ -9,17 +10,17 @@ const HomePage = () => {
         <div className="card-container">
           <div className="card-body">
             <div className="card-title">
-              <img className="person-photo" src="" alt="person face" />
-              <h5 className="person-name">Chiranshu</h5>
+              <img className="person-photo" src={Logo} alt="person face" />
+              <h5 className="person-name">Chiranshu Agrawal</h5>
             </div>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+            <p className="card-text mt-3">
+            The college playground serves as a catalyst for personal growth and community engagement, leaving an indelible mark on those who partake. Through shared experiences and moments of joy, it fosters connections and empowers individuals to embrace their potential.
             </p>
             <p className="card-text">
               <small className="text-body-secondary">Last updated 3 mins ago</small>
             </p>
           </div>
-          <img className="card-img-bottom" src="" alt="post" />
+          <img className="card-img-bottom" src={Logo1} alt="post" />
         </div>
       </div>
     </div>
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Logo from "../images/Logo-jiit.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { initFlowbite } from "flowbite";
 const Navbar = () => {
   initFlowbite();
+  let location = useLocation();
   return (
     <div>
       <nav className="border-gray-200 dark:bg-gray-900">
@@ -117,7 +118,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/homepage"
-                  className="ml-2 block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  className={`nav-link ${location.pathname === "/homepage" ? "ml-2 block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" : ""}`}
                   aria-current="page"
                 >
                   Home
@@ -126,7 +127,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/grownnetwork"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={`nav-link ${location.pathname === "/grownnetwork" ? "ml-2 block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" : ""}`}
                 >
                   Grow Network
                 </Link>
@@ -134,7 +135,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/jobopening"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={`nav-link ${location.pathname === "/jobopening" ? "ml-2 block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" : ""}`}
                 >
                   Job Openings
                 </Link>
@@ -142,7 +143,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/resume"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={`nav-link ${location.pathname === "/resume" ? "ml-2 block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" : ""}`}
                 >
                   Resume Improvement
                 </Link>
@@ -150,7 +151,7 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/askforreferal"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className={`nav-link ${location.pathname === "/askforreferal" ? "ml-2 block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" : ""}`}
                 >
                   Ask For Referral
                 </Link>
