fc6728f5efa184d5cc3081e544b2d020b48396d3
Chiranshu Agrawal <agarwalchiranshu2003@gmail.com>
2024-10-18 23:58:25 +0530

    Admin HomePage Updated

--- frontend/src/Components/AdminHomePage.js
+++ frontend/src/Components/AdminHomePage.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import AdminSidebar from './AdminSidebar';
+
 
 const AdminHomePage = () => {
   return (
-    <div>
-      Admin Logged In
+    <div className="flex min-h-screen">
+      <AdminSidebar />
+      <div className="flex-1 p-10 bg-gray-100">
+          <div>
+              <h3 className="text-2xl font-semibold mb-4">Post Articles</h3>
+              {/* Add your Article Post form/component here */}
+              <form>
+                  {/* Example fields */}
+                  <input type="text" placeholder="Article Title" className="border p-2 mb-4 w-full" />
+                  <textarea placeholder="Article Content" className="border p-2 mb-4 w-full" />
+                  <button type="submit" className="bg-teal-500 text-white p-2">Post Article</button>
+              </form>
+          </div>
+      </div>
+
     </div>
-  )
-}
+  );
+};
 
 export default AdminHomePage;
